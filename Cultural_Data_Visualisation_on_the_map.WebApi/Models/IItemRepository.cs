ab5b8b00ad69b1e61396edbe5d4af38d8d682148
AndrewGronin <you@example.com>
2019-11-23 11:28:38 +0300

    Добавьте файлы проекта.

---
+++ Cultural_Data_Visualisation_on_the_map.WebApi/Models/IItemRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Cultural_Data_Visualisation_on_the_map.Core.Models;
+
+namespace Cultural_Data_Visualisation_on_the_map.WebApi.Models
+{
+    // TODO WTS: Replace or update this interface when no longer using sample data.
+    public interface IItemRepository
+    {
+        void Add(SampleCompany item);
+
+        void Update(SampleCompany item);
+
+        SampleCompany Remove(string id);
+
+        SampleCompany Get(string id);
+
+        IEnumerable<SampleCompany> GetAll();
+    }
+}
---
+++ Cultural_Data_Visualisation_on_the_map/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace Cultural_Data_Visualisation_on_the_map.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
